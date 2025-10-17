08d8061c182df94e42596b76f492d22d1c0275be
Mauricio650 <mauricioibanez650@gmail.com>
2025-10-17 15:50:42 -0500

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import 'primereact/resources/primereact.min.css' // Estilos base de PrimeReact
 import 'primeicons/primeicons.css'
@@ -8,7 +9,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-
+    <Analytics></Analytics>
     <App />
 
   </BrowserRouter>
